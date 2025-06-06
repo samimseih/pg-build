b9464d1bc0331886a8555aa43613784c49a40dc0
Ubuntu <ubuntu@ip-172-31-38-230.ec2.internal>
2025-06-06 20:12:17 +0000

    added example.sh that can be symlinked

---
+++ example.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Resolve the real path of the script (follows symlinks)
+SOURCE="${BASH_SOURCE[0]}"
+while [ -L "$SOURCE" ]; do
+  DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ "$SOURCE" != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+SCRIPT_DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
+
+python $SCRIPT_DIR/pg_build.py \
+--source /home/ubuntu/postgres.tar.gz \
+--meson-flags="-Ddocs=enabled -Duuid=e2fs --debug -Dcassert=true -Dtap_tests=enabled -Dc_args=-fno-omit-frame-pointer" \
+"$@"
