6f911ecca6dd7eef3f8d85f16feae91cdc80ddc4
Ubuntu <ubuntu@ip-172-31-46-230.ec2.internal>
2025-10-30 13:14:41 +0000

    modified example

--- example.sh
+++ example.sh
@@ -11,5 +11,6 @@ SCRIPT_DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
 
 python3 $SCRIPT_DIR/pg_build.py \
 --source postgres.tar.gz \
---meson-flags="-Ddocs=enabled -Duuid=e2fs --debug -Dcassert=true -Dtap_tests=enabled -Dc_args=-fno-omit-frame-pointer" \
+--meson-flags="-Ddocs=enabled -Duuid=e2fs --debug -Dcassert=true -Dtap_tests=enabled -Dinjection_points=true" \
 "$@"
+
