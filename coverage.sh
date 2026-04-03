3cbc79475f4f8169f3eb76f81fc3cc784093c563
Sami Imseih <samimseih@gmail.com>
2026-04-03 15:49:33 +0000

    coverage.sh: derive source dir from build dir and ignore lcov errors
    - Derive SOURCE_DIR from BUILD_DIR using dirname, reducing args from 3 to 2
    - Add --ignore-errors unmapped to genhtml for flex/bison generated files
    - Add --ignore-errors unused to lcov --remove and genhtml

--- coverage.sh
+++ coverage.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # pg_coverage.sh - Generate an HTML coverage report for PostgreSQL
-# Usage: ./pg_coverage.sh <build_dir> <source_dir> [output_dir]
+# Usage: ./pg_coverage.sh <build_dir> [output_dir]
 
 sudo yum install lcov -y
 
-BUILD_DIR="${1:?Usage: $0 <build_dir> <source_dir> [output_dir]}"
-SOURCE_DIR="${2:?Usage: $0 <build_dir> <source_dir> [output_dir]}"
-OUTPUT_DIR="${3:-coverage_report}"
+BUILD_DIR="${1:?Usage: $0 <build_dir> [output_dir]}"
+SOURCE_DIR="$(dirname "$BUILD_DIR")"
+OUTPUT_DIR="${2:-coverage_report}"
 LCOV_FILE="$OUTPUT_DIR/coverage.info"
 
 set -e
@@ -24,9 +24,9 @@ lcov --capture --directory "$BUILD_DIR" --output-file "$LCOV_FILE.test" \
 lcov -a "$LCOV_FILE.base" -a "$LCOV_FILE.test" -o "$LCOV_FILE"
 
 # Remove noise
-lcov --remove "$LCOV_FILE" '*/src/test/*' '*/contrib/*' '*/src/interfaces/ecpg/test/*' -o "$LCOV_FILE"
+lcov --remove "$LCOV_FILE" '*/src/test/*' '*/contrib/*' '*/src/interfaces/ecpg/test/*' -o "$LCOV_FILE" --ignore-errors unused
 
 # Generate HTML
-genhtml "$LCOV_FILE" --output-directory "$OUTPUT_DIR" --title "PostgreSQL Coverage"
+genhtml "$LCOV_FILE" --output-directory "$OUTPUT_DIR" --title "PostgreSQL Coverage" --ignore-errors unmapped,unused
 
 echo "Report: $OUTPUT_DIR/index.html"
